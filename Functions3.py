9488d245c58a525835596df064f117303c9e1cd7
LauraBarrio <36167049+LauraBarrio@users.noreply.github.com>
2018-02-05 19:43:19 +0100

    Add files via upload

---
+++ Functions3.py
@@ -0,0 +1,387 @@
+"""
+@author: M. Palmer
+
+Module that contains the transformations needed to determine MLE (please refer to Xavier Luri Carrascoso PhD thesis (1993)
+Un Nuevo metodo de maxima verosimilitud para la determinacion de magnitudes absolutas
+"""
+import sys
+import time
+import csv
+import random
+import copy
+import scipy
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+#import matplotlib.mlab as mlab
+import numdifftools as nd
+
+from numpy import matrix
+from scipy import interpolate
+from math import pow, pi, sqrt, log, log10, exp, sin, cos, atan2, asin, radians, degrees
+from scipy.integrate import quad,dblquad,romberg
+from scipy.special import erfc
+from multiprocessing import Process, Queue
+from scipy.optimize import minimize
+
+
+# Options: Cluster, kinematics, catalogue
+# TODO: This needs some refactoring
+cluster="Hyades" # cluster name
+kinimatics=False   # True or False
+cat="New"          # New or Old
+
+
+# If cluster is Pleiades or Hyades (age)
+if cluster=="Pleiades": AgCluster = 0.0975
+else:  AgCluster =0.0
+print "Cluster Extinction is", AgCluster
+k=4.74            
+Dg=0.4782
+Ag=4.9291
+
+##BINS (hardcoded)
+# Color bins for the Pleiades cluster (original bins using bp-rp)
+#if cluster=="Pleiades": bins = [[-0.1, 0.0], [0.0, 0.4], [0.4, 0.6], [0.6, 0.8]]
+# Color bins using bt-vt
+if cluster=="Pleiades": bins = [[-0.1, 0.5], [0.5, 1.5], [1.5, 2.5], [2.5, 3.6]]
+if cluster=="PleiadesGeneva": bins = [[-1, -0.8], [-0.8, -0.75], [-0.75, -0.5], [-0.5, -0.1]]
+
+# Color bins for the Hyades cluster, bp-rp and bt-vt range is almost identical
+elif cluster=="Hyades": bins = [[0.3,0.7],[0.7,1.1],[1.1,1.5],[1.5,1.9]]
+
+#color bins for Alpha Persei (bt-vt)
+elif cluster=="Alpha_persei_1": bins=[[-0.1,0.2],[0.2,0.5],[0.5,0.8],[0.8,1.1]]
+
+#color bins for Blanco 1 (bt-vt)
+elif cluster=="Blanco1": bins=[[-0.1,0.2],[0.2,0.5],[0.5,0.8],[0.8,1.1]]
+
+#color bins for coll140 (bt-vt)
+elif cluster=="Col140_1": bins=[[-0.25,0],[0,0.2],[0.2,0.4],[0.4,0.7]]
+
+#color bins for Coma berenices (bt-vt)
+elif cluster=="Coma_berenices_1": bins=[[0.1,0.45],[0.45,0.8],[0.8,1.15],[1.15,1.5]]
+
+#color bins for IC2391 (bt-vt)
+elif cluster=="IC2391": bins=[[-0.2,0.1],[0.1,0.4],[0.4,0.7],[0.7,1.0]]
+
+#color bins for IC2602 (bt-vt)
+elif cluster=="IC2602_1": bins=[[-0.2,0.375],[0.375,0.95],[0.95,1.525],[1.525,2.1]]
+
+#color bins for IC4665 (bt-vt)
+elif cluster=="IC4665_1": bins=[[-0.1,0.1],[0.1,0.3],[0.3,0.5],[0.5,0.7]]
+
+#color bins for NGC2232 (bt-vt)
+elif cluster=="NGC2232": bins=[[-0.2,0.0],[0.0,0.2],[0.2,0.5],[0.5,0.9]]
+
+#color bins for NGC2422 (bt-vt)
+elif cluster=="NGC2422_1": bins=[[-0.1,0.0],[0.0,0.2],[0.2,0.4],[0.4,0.6]]
+
+#color bins for NGC2451(bt-vt)
+elif cluster=="NGC2451_1": bins=[[-0.2,0.1],[0.1,0.4],[0.4,0.7],[0.7,1]]
+
+#color bins for NGC2516 (bt-vt)
+elif cluster=="NGC2516_1": bins=[[-0.1,0.0],[0.0,0.2],[0.2,0.4],[0.4,0.7]]
+
+#color bins for NGC2547 (bt-vt)
+elif cluster=="NGC2547": bins=[[-0.15,0.0],[0.0,0.15],[0.15,0.3],[0.3,0.5]]
+
+#color bins for NGC3532 (bt-vt)
+elif cluster=="NGC3532_1": bins=[[-0.2,0],[0,0.2],[0.2,0.4],[0.4,0.8]]
+
+#color bins for NGC6475 (bt-vt)
+elif cluster=="NGC6475": bins=[[-0.05,0.1],[0.1,0.2],[0.2,0.4],[0.4,1.1]]
+
+#color bins for NGC6633 (bt-vt)
+elif cluster=="NGC6633_1": bins=[[-0.01,0.2],[0.2,0.3],[0.3,0.6],[0.6,0.8]]
+    #bins=[[-0.01,0.4],[0.4,0.8]]
+
+#color bins for NGC7092 (bt-vt)
+elif cluster=="NGC7092_1": bins=[[-0.04,0.05],[0.05,0.15],[0.15,0.25],[0.25,0.5]]
+
+#color bins for Praesepe (bt-vt)
+elif cluster=="Praesepe_1": bins=[[0,0.25],[0.25,0.5],[0.5,0.75],[0.75,1.0]]
+
+
+
+# Transformations (refer to Xavi's PhD thesis)
+# TODO: pending documentation
+def Fv(p,s,r0): return (K3(p,s,r0)) *exp ((Y(p,s,r0)**2/(4*X(p,s,r0))) - Z(p,s,r0))   # P is Params array, S is star data array
+def K3(p,s,r0): return sqrt(- pi**3 / (J2(p,s,r0)*E1(p,s,r0)*X(p,s,r0) ))
+def X(p,s,r0):  return (D1(p,s,r0)**2/(4*E1(p,s,r0)))-F1(p,s,r0)
+def Y(p,s,r0):  return ((B1(p,s,r0)*D1(p,s,r0))/(2*E1(p,s,r0)))-C1(p,s,r0)
+def Z(p,s,r0):  return ( pow(B1(p,s,r0),2)/(4*E1(p,s,r0)))-A1(p,s,r0)
+def A1(p,s,r0): return ( pow(A2(p,s,r0),2)/(4*J2(p,s,r0)))-D2(p,s,r0)
+def B1(p,s,r0): return ((A2(p,s,r0)*B2(p,s,r0))/(2*J2(p,s,r0))) - E2(p,s,r0)
+def C1(p,s,r0): return ((A2(p,s,r0)*C2(p,s,r0))/(2*J2(p,s,r0))) - F2(p,s,r0)
+def D1(p,s,r0): return ((B2(p,s,r0)*C2(p,s,r0))/(2*J2(p,s,r0))) - G2(p,s,r0)
+def E1(p,s,r0): return ( pow(B2(p,s,r0),2)/(4*J2(p,s,r0)))-H2(p,s,r0)
+def F1(p,s,r0): return ( pow(C2(p,s,r0),2)/(4*J2(p,s,r0)))-I2(p,s,r0)
+
+def A2(p,s,r0): return -(s['muAlpha']/ pow(s['epsMuAlpha'],2)) - ((  ((a1(s)*p['U'])/ pow(p['sigmaU'],2)) + ((a2(s)*p['V'])/ pow(p['sigmaV'],2)) + ((a3(s)*p['W'])/ pow(p['sigmaW'],2))  ) *r0)
+def B2(p,s,r0): return (  ((a1(s)*b1(s))/ pow(p['sigmaU'],2)) + ((a2(s)*b2(s))/ pow(p['sigmaV'],2)) + ((a3(s)*b3(s))/ pow(p['sigmaW'],2))  ) *  pow(r0,2) 
+def C2(p,s,r0): return (  ((a1(s)*c1(s))/ pow(p['sigmaU'],2)) + ((a2(s)*c2(s))/ pow(p['sigmaV'],2)) + ((a3(s)*c3(s))/ pow(p['sigmaW'],2))  ) *r0
+def D2(p,s,r0): return 0.5 * ((pow(s['muAlpha'],2)/pow(s['epsMuAlpha'],2)) + (pow(s['muDelta'],2)/pow(s['epsMuDelta'],2)) + (pow(s['vr'],2)/pow(s['epsVr'],2)) + (pow(p['U'],2)/pow(p['sigmaU'],2)) + (pow(p['V'],2)/pow(p['sigmaV'],2)) + (pow(p['W'],2)/pow(p['sigmaW'],2))) 
+def E2(p,s,r0): return -(s['muDelta']/ pow(s['epsMuDelta'],2)) - (( ((b1(s)*p['U'])/pow(p['sigmaU'],2)) + ((b2(s)*p['V'])/pow(p['sigmaV'],2)) + ((b3(s)*p['W'])/pow(p['sigmaW'],2))) *r0)
+def F2(p,s,r0): return -(s['vr']/ pow(s['epsVr'],2)) - ( ((c1(s)*p['U'])/pow(p['sigmaU'],2)) + ((c2(s)*p['V'])/pow(p['sigmaV'],2)) + ((c3(s)*p['W'])/pow(p['sigmaW'],2))  )
+def G2(p,s,r0): return (  ((b1(s)*c1(s))/pow(p['sigmaU'],2)) + ((b2(s)*c2(s))/pow(p['sigmaV'],2)) + ((b3(s)*c3(s))/pow(p['sigmaW'],2)) ) *r0
+def H2(p,s,r0): return (1.0/(2*pow(s['epsMuDelta'],2))) + 0.5*(  (( pow(b1(s),2))/( pow(p['sigmaU'],2)))+(( pow(b2(s),2))/( pow(p['sigmaV'],2)))+(( pow(b3(s),2))/( pow(p['sigmaW'],2))))* pow(r0,2)    
+def I2(p,s,r0): return (1.0/(2*pow(s['epsVr'],2)))  + 0.5*(  (( pow(c1(s),2))/( pow(p['sigmaU'],2)))+(( pow(c2(s),2))/( pow(p['sigmaV'],2)))+(( pow(c3(s),2))/( pow(p['sigmaW'],2))))
+def J2(p,s,r0): return (1.0/(2*pow(s['epsMuAlpha'],2))) + 0.5*(  (( pow(a1(s),2))/( pow(p['sigmaU'],2)))+(( pow(a2(s),2))/( pow(p['sigmaV'],2)))+(( pow(a3(s),2))/( pow(p['sigmaW'],2))))* pow(r0,2)    
+
+def a1(s): return k*(s['sfi']*s['cl']*s['sb']-s['cfi']*s['sl'])
+def a2(s): return k*(s['sfi']*s['sl']*s['sb']+s['cfi']*s['cl'])
+def a3(s): return -k*s['sfi']*s['cb']
+def b1(s): return -k*(s['cfi']*s['cl']*s['sb']+s['sfi']*s['sl'])
+def b2(s): return -k*(s['cfi']*s['sl']*s['sb']-s['sfi']*s['cl'])
+def b3(s): return k*s['cfi']*s['cb']
+def c1(s): return s['cl']*s['cb']
+def c2(s): return s['sl']*s['cb']
+def c3(s): return s['sb']
+
+def getU(a,s): return a1(a)*s['muAlpha']/s['pi']  +  b1(a)*s['muDelta']/s['pi']  +  c1(a)*s['vr']
+def getV(a,s): return a2(a)*s['muAlpha']/s['pi']  +  b2(a)*s['muDelta']/s['pi']  +  c2(a)*s['vr']
+def getW(a,s): return a3(a)*s['muAlpha']/s['pi']  +  b3(a)*s['muDelta']/s['pi']  +  c3(a)*s['vr']
+
+def calculateMeanL(slist, N): return  atan2(sum([item['sinl'] for item in slist]) / N, sum([item['cosl'] for item in slist]) / N)
+def calculateMeanB(slist, N): return  atan2(sum([item['sinb'] for item in slist]) / N, sum([item['cosb'] for item in slist]) / N)
+
+
+def calculateMeanR(slist, N):
+    '''
+    Calculates the mean R
+    :param slist:
+    :param N:
+    :return:
+    '''
+    r = 0
+    for item in slist:
+        r = r + (1.0 / item['pi'])
+    rmean = r / N
+    return rmean
+
+def Fi(a,l,b):
+    '''
+
+    :param a:
+    :param l:
+    :param b:
+    :return:
+    '''
+    x=cos(Dg)*sin(getDelta(a,l,b))*sin(getAlpha(a,l,b)-Ag)+sin(Dg)*cos(getDelta(a,l,b))
+    y=cos(Dg)*cos(getAlpha(a,l,b)-Ag)
+    return atan2(y,x)
+
+def getA(l,b):
+    '''
+
+    :param l:
+    :param b:
+    :return:
+    '''
+    a={'sb':sin(b),'cb':cos(b),'sl':sin(l),'cl':cos(l)}
+    a.update({'cfi':cos(Fi(a,l,b)),'sfi':sin(Fi(a,l,b))})
+    return a
+
+def getDelta(a,l,b): return asin(a['cb']*sin(l-33*pi/180)*sin(62.6*pi/180)+a['sb']*cos(62.6*pi/180))
+
+def getAlpha(a,l,b):
+    '''
+
+    :param a:
+    :param l:
+    :param b:
+    :return:
+    '''
+    t1=(a['cb']*sin(l-33*pi/180)*cos(62.6*pi/180))-(a['sb']*sin(62.6*pi/180))
+    t2=a['cb']*cos(l-33*pi/180)
+    return atan2(t1,t2) +Ag
+
+def getVr(a,U,V,W): return (W- U*a3(a)/a1(a) - (V-U*a2(a)/a1(a)) * (b3(a)-b1(a)*a3(a)/a1(a)) / (b2(a)-b1(a)*a2(a)/a1(a))) /(( c3(a)-c1(a)*a3(a)/a1(a) )-(c2(a)-c1(a)*a2(a)/a1(a)) * (b3(a)-b1(a)*a3(a)/a1(a)) / (b2(a)-b1(a)*a2(a)/a1(a)))
+def getMuDelta(a,V,U,vr,r): return ((V-U*a2(a)/a1(a) ) - (( c2(a)-c1(a)*a2(a)/a1(a) ) * vr)) / r / ( b2(a)-b1(a)*a2(a)/a1(a) )
+def getMuAlpha(a,U,vr,r,muDelta): return  (U-r*b1(a)*muDelta-c1(a)*vr)/r/a1(a)
+
+def logD(D,star):
+    '''
+
+    :param D:
+    :param star:
+    :return:
+    '''
+    if D > 0:     D =  log(D)
+    elif D == 0:  D = -10000
+    elif D < 0: 
+        print "Error: negative D"
+        sys.exit()
+    return D    
+
+def transformCoordinates(s,c):
+    '''
+
+    :param s:
+    :param c:
+    :return:
+    '''
+    s['lprime'] = s['l'] - c['lmean']
+    if s['lprime'] < 0: s['lprime'] = 2 *  pi + s['lprime']
+    if (- pi / 2) - 0.2 > c['bmean'] or ( pi / 2) - 0.2 < c['bmean']  : print "WARNING: Close to anticenter... check coordinate rotation", c['bmean'], s['l'], s['b']
+    s['bprime'] = s['b'] - c['bmean']
+    s.update({'sb':sin(s['b']),'cb':cos(s['b']),'sl':sin(s['l']),'cl':cos(s['l']),'sbprime':sin(s['bprime']),'cbprime':cos(s['bprime']),'slprime':sin(s['lprime']),'clprime':cos(s['lprime'])})
+    a=getA(s['l'],s['b'])
+    s.update({'cfi':cos(Fi(a,s['l'],s['b'])),'sfi':sin(Fi(a,s['l'],s['b']))})  
+    return s
+
+
+def getP(x):
+    '''
+
+    :param x:
+    :return:
+    '''
+    if kinimatics==True:
+        labels =  ['R','sS1','sS2','sS3','sS4','x1','x2','x3','x4','x5','sM1','sM2','sM3','sM4','U','V','W','sigmaUVW'] 
+        p=dict(zip(labels, x))
+        p.update({'R^2':p['R']**2,"sigmaU":p['sigmaUVW'],"sigmaV":p['sigmaUVW'],"sigmaW":p['sigmaUVW'] })
+    elif kinimatics==False: 
+        labels =  ['R','sS1','sS2','sS3','sS4','x1','x2','x3','x4','x5','sM1','sM2','sM3','sM4','muAlphaMean','muDeltaMean','sigmaMuAlpha','sigmaMuDelta'] 
+        p=dict(zip(labels, x))
+        p.update({'R^2':p['R']**2})           
+    return p   
+
+def checkParamsAreValid(p):
+    '''
+
+    :param p:
+    :return:
+    '''
+    if kinimatics==True:
+        if min(p['sS1'],p['sS2'],p['sS3'],p['sS4'],p['sM1'],p['sM2'],p['sM3'],p['sM4'],p['R'],p['sigmaUVW'])<=0:
+            return False
+        else: return True
+    elif kinimatics==False:
+        if min(p['sS1'],p['sS2'],p['sS3'],p['sS4'],p['sM1'],p['sM2'],p['sM3'],p['sM4'],p['R'],p['sigmaMuAlpha'],p['sigmaMuDelta'])<=0:
+            return False
+        else: return True
+        
+        
+def startProcess(procs, target, args):
+    '''
+
+    :param procs:
+    :param target:
+    :param args:
+    :return:
+    '''
+    thisProcess = Process(target=target, args=args)
+    procs.append(thisProcess)
+    thisProcess.start()
+    return procs
+
+def getSigmaSSigmaMforC(p,minColour,maxColour):
+    '''
+
+    :param p:
+    :param minColour:
+    :param maxColour:
+    :return:
+    '''
+    for i in xrange(0,len(bins),1):
+        if minColour == bins[i][0] and maxColour == bins[i][1]:
+            sigmaS = p['sS%s' % str(i+1)]
+            sigmaM = p['sM%s' % str(i+1)]
+    return sigmaS, sigmaM 
+
+def getSigmaSSigmaMforD(p,star):
+    '''
+
+    :param p:
+    :param star:
+    :return:
+    '''
+    sigmaS=None
+    for i in xrange(0,len(bins),1):
+        if star['bp-rp'] >= bins[i][0] and star['bp-rp'] < bins[i][1]:
+            sigmaS = p['sS%s' % str(i+1)]
+            sigmaM = p['sM%s' % str(i+1)]
+    return sigmaS, sigmaM 
+
+def getCminCmaxforD(star):
+    '''
+
+    :param star:
+    :return:
+    '''
+    for thisBin in bins:
+        if star['bp-rp'] >= thisBin[0] and star['bp-rp'] < thisBin[1]: 
+            return thisBin[0],thisBin[1] 
+
+def getCminCmaxforC(minColour,maxColour):
+    '''
+
+    :param minColour:
+    :param maxColour:
+    :return:
+    '''
+    for thisBin in bins:
+        if minColour == thisBin[0] and maxColour == thisBin[1]: 
+            return thisBin[0],thisBin[1] 
+
+def getWeight(c,minColour,maxColour):
+    '''
+
+    :param c:
+    :param minColour:
+    :param maxColour:
+    :return:
+    '''
+    for i in xrange(0,len(bins),1):
+        if minColour == bins[i][0] and maxColour == bins[i][1]:
+            w=c['w']
+            return w[i]       
+
+# Distributions for magnitud, space, velocity, dispersion in distance and proper motions
+def Fmag(star,r0,spline,sigmaM): return exp(-0.5 *  pow((star['mg'] - (5*log10(r0)) + 5 - AgCluster - (interpolate.splev(star['bp-rp'],spline))) / sigmaM, 2))
+def Fspace(star,r0,p,sigmaS): return exp (-0.5 * (p['R^2'] + pow(r0,2) - (2 * r0 * p['R'] *  star['cbprime'] * star['clprime'])) / (sigmaS**2) )
+#def Fvel(star,muAlpha0,muDelta0,p): return exp (-0.5 *  pow((muAlpha0 - p['muAlphaMean']) / p['sigmaMuAlpha'], 2)) * exp (-0.5 *  pow((muDelta0 - p['muDeltaMean']) / p['sigmaMuDelta'], 2)) 
+def Fvel(star,muAlpha0,muDelta0,p): return exp (-0.5 *  pow((muAlpha0 - p['muAlphaMean']) / p['sigmaMuAlpha'], 2)) * exp (-0.5 *  pow((muDelta0 - p['muDeltaMean']) / p['sigmaMuDelta'], 2)) 
+
+
+def jacobian(star,r0): return pow(r0,2)* star['cbprime'] 
+def epsPi(star,r0): return exp (-0.5 *  pow((star['pi'] - (1.0 / r0)) / star['epsPi'], 2))
+def epsMuAlpha(star,muAlpha0): return exp (-0.5 *  pow((star['muAlpha'] -  muAlpha0) / star['epsMuAlpha'], 2)) 
+def epsMuDelta(star,muDelta0): return exp (-0.5 *  pow((star['muDelta'] -  muDelta0) / star['epsMuDelta'], 2)) 
+
+
+def getSpline(bins,p):
+    '''
+    Spline function which gives the isochrone
+    :param bins:
+    :param p:
+    :return:
+    '''
+    y=[p['x1']]
+    x=[bins[0][0]]
+    for i in bins:
+        x.append(i[1])
+    for i in xrange(0,len(bins),1):
+        y.append(p['x%s' %str(i+2)])
+    return interpolate.splrep(x,y)
+
+
+def findW(starlist):
+    '''
+    Number of stars inside color bins
+    :param starlist:
+    :return:
+    '''
+    w=[0,0,0,0]
+    for star in starlist:
+        for i in xrange(0,len(bins)):
+            if star['bp-rp']<bins[i][1] and star['bp-rp'] > bins[i][0]: 
+                w[i]=w[i]+1
+    print "number per bin",w
+    for i in xrange(0,len(w)):
+        w[i]=float(w[i])/len(starlist)
+    print "weightings",w
+    return w
+
---
+++ ML3.py
@@ -0,0 +1,291 @@
+"""
+@author: M. Palmer
+
+Computes the ML estimates of the parameters of the model being the stars belonging to a cluster which is spherical at
+some distance, with the absolute magnitude following a isochrone and moving trough the space with some velocity.
+
+Input: a file with a list of cluster members with the following parameters:
+magnitude G        = mg (mag)
+galatic latitude  = l (radians)
+galatic longitude = b (radians)
+parallax          = pi (muas)
+parallax error    = epsPi (muas)
+mualpha           = muAlpha (muas)
+mualpha error     = epsMuAlpha (muas)
+mudelta           = muDelta (muas)
+mudelta error     = epsMuDelta (muas)
+color             = bp-rp (mag) (bt-vt can be used)
+
+Output:
+Parameters, distance, position, speed, size isochrone, with their errors
+
+"""
+from Functions3 import *
+random.seed(1)
+
+def calculateErrors(starlist, c, res):
+    '''
+    Finds the derivative of the likelihood function around the maximum computing a Hessian matrix
+    (Please refer to M. Palmer et al (2014)
+
+    :param starlist: star list
+    :param c: star dictionary with number of stars, Gaia mag limit, lMean, bMean, w
+    :param res: MLE results
+    :return: covariance matrix
+    '''
+
+    print "\n\nCalculating errors...\n\n"
+    
+    def callML(x):
+        p=getP(x)
+        return  LikelihoodFunction(starlist, p,c)
+
+    def makearray(value, nrows, ncols):
+        return [[value]*ncols for _ in range(nrows)]
+
+    cov = makearray(None,16,16)
+    error=[]
+    Hfun = nd.Hessian(callML)
+    tempMatrix = matrix(Hfun(res))
+
+    print tempMatrix
+    covarianceMatrix = np.array(tempMatrix.I).tolist()
+    print "\nCovariance Matrix:\n", covarianceMatrix   
+    
+    for i in xrange(0,len(res),1):
+        try: error.append( sqrt(-covarianceMatrix[i][i]))
+        except: 
+            error.append("Error")
+            print "Hessian Error"         
+    print "Errors are:",error
+
+
+    for i in xrange(0,len(res),1):
+        for j in xrange(0,len(res),1):
+            try:
+                cov[i][j]= covarianceMatrix[i][j] / (error[i] * error[j])    
+            except:
+                print i,j
+                cov[i][j]="Error"
+    print "correlations are",cov    
+    
+            
+def calculatesNormalizationCoeff(p, c, minColour, maxColour, spline, outputC):
+    '''
+    Calculates the normalisation coefficient
+    :param p:
+    :param c:
+    :param minColour:
+    :param maxColour:
+    :param spline:
+    :param outputC:
+    :return: nothing
+    '''
+    sigmaS, sigmaM =  getSigmaSSigmaMforC(p,minColour,maxColour)
+    weight = getWeight(c,minColour,maxColour)
+    
+    def Ir(r0):
+        def Ib(b0): 
+            def Il(l0): return  exp((-0.5 * (p['R^2']  + r0 ** 2 - 2 * r0 * p['R'] *  cos(b0) *  cos(l0)) / (pow(sigmaS,2)) ))
+            integralL = quad(Il, -0.04, 0.04)
+            return integralL[0] *  cos(b0) 
+        def Ibprp(bprp):  
+            Mh = interpolate.splev(bprp,spline)
+            m0= Mh + (5*log10(r0)) - 5 + AgCluster
+            p = sigmaM*erfc(-((c['mlim']- m0)  /(sqrt(2)*sigmaM)))
+            return p
+        
+        integralB = quad(Ib, -0.04, 0.04)
+        integralBPRP= quad(Ibprp, minColour, maxColour)
+        
+        return integralB[0] * r0 ** 2 *  integralBPRP[0]
+    integralR = quad(Ir,10, p['R'] + 100, epsabs =0)
+
+    value =  0.5*(2*pi)**4 * p['sigmaMuAlpha'] * p['sigmaMuDelta']  * integralR[0] *weight #PUT WEIGHT BACK IN HERE !!!
+    outputC.put(value)
+
+def calculatesUnnormaLikelihood(p, c, star, spline, outputstars, CsinEps):
+    '''
+    Calculates the unnormalized Likelihood for each star
+    :param p:
+    :param c:
+    :param star:
+    :param spline:
+    :param outputstars:
+    :param CsinEps:
+    :return:
+    '''
+    sigmaS, sigmaM =  getSigmaSSigmaMforD(p,star)
+    thisCmin, thisCmax = getCminCmaxforD(star)
+    weight = getWeight(c,thisCmin,thisCmax)
+
+    def Imu(muAlpha0,muDelta0): return Fvel(star,muAlpha0,muDelta0,p)  * epsMuAlpha(star,muAlpha0) * epsMuDelta(star,muDelta0)
+#    integralMu = dblquad(Imu,-0.07,-0.03,lambda x: 0,lambda x: 0.05,epsabs=0)    
+    integralMu = dblquad(Imu, p['muDeltaMean']-0.025, p['muDeltaMean']+0.025, lambda x: p['muAlphaMean']-0.025, lambda x: p['muAlphaMean']+0.025, epsabs=0)
+    
+    def Ir(r0): return jacobian(star,r0) * Fspace(star,r0,p,sigmaS) * Fmag(star,r0,spline,sigmaM) * epsPi(star,r0) * integralMu[0]
+    integralR = quad(Ir,10, p['R']+50)
+    outputstars.put( weight *  integralR[0]  / (CsinEps * star['epsPi'] *star['epsMuAlpha'] * star['epsMuDelta']))  
+
+def LikelihoodFunction(starlist, p, c):
+    '''
+    Calculates the value of the likelihood functin for every star
+    :param starlist:
+    :param p:
+    :param c:
+    :return:
+    '''
+    if checkParamsAreValid(p) == False: 
+        print "The Sum: -10E307 \n"
+        return -10E307  
+    else:
+        outputC, outputstars, procs, Plist = Queue(), Queue(), [], []
+        spline = getSpline(bins,p)
+        
+        for colourRange in bins:
+            procs = startProcess(procs, target=calculatesNormalizationCoeff, args=(p, c, colourRange[0], colourRange[1], spline, outputC))
+        CsinEps = outputC.get()+outputC.get()+outputC.get()+outputC.get()
+        for P in procs: P.join()
+        procs=[]
+        
+        for star in starlist:
+            star=transformCoordinates(star,c)
+            procs = startProcess(procs, target=calculatesUnnormaLikelihood, args=(p, c, star, spline, outputstars, CsinEps))
+        
+        for P in procs: P.join()
+        
+        
+        for i in xrange (0,len(starlist),1):  Plist.append(  logD(outputstars.get(),star)  )
+        finalSum = sum(Plist)
+        print "The Sum:",finalSum,"\n"        
+        return finalSum
+
+
+def MLE(starlist, c, guess):
+    '''
+    MLE wrapper
+
+    :param starlist:
+    :param c:
+    :param guess:
+    :return:
+    '''
+    t0 = time.time()
+    print "\nMean coordinates are", c['lmean'], c['bmean']
+
+    def f(x):      
+        print x
+        p = getP(x)
+        return -LikelihoodFunction(starlist, p, c) 
+    
+    res =  minimize(f, x0=guess,method="Powell")
+    print "NM: " , res.message, "\n", res.x, "\n", "Total time:", time.time() - t0
+    return res.x   
+
+
+"""
+
+Main program to calculate ML. Catalogue should be located inside the same directory
+
+"""
+if __name__ == '__main__':
+
+
+    # Reads a csv file containing the parameters
+    fileName = 'Hyades'
+    starlist = pd.read_csv(fileName, names=['mg', 'l', 'b', 'pi', 'epsPi',
+                                              'muAlpha', 'epsMuAlpha', 'muDelta',
+                                              'epsMuDelta', 'bp-rp']).T.to_dict().values()
+
+    # Coordinate transformation to 3 dimensional space x, y and z
+    for star in starlist:
+        star['sinl'] = sin(star['l'])
+        star['cosl'] = cos(star['l'])
+        star['sinb'] = sin(star['b'])
+        star['cosb'] = cos(star['b'])
+        star['x'] = (1.0 / star['pi']) * star['cosb'] * star['cosl']
+        star['y'] = (1.0 / star['pi']) * star['cosb'] * star['sinl']
+        star['z'] = (1.0 / star['pi']) * star['sinb']
+
+    # In case of having radial velocities
+    #for i in range(len(starlist)):
+    #    starlist[i].pop('vr', None)
+    #    starlist[i].pop('epsVr-V', None)
+
+
+    distlist,maxlist,minlist,llist,blist,xlist,ylist,zlist = [],[],[],[],[],[],[],[]
+
+    # Creates list of values for each parameter
+    for star in starlist:
+        if star['pi']>0: 
+            distlist.append(1/star['pi'])
+            llist.append(star['l'])
+            blist.append(star['b'])
+            maxlist.append((1/star['pi'])-(1/(star['pi']-star["epsPi"])))
+            minlist.append(-((1/star['pi'])-(1/(star['pi']+star["epsPi"]))))
+            xlist.append(star['x'])
+            ylist.append(star['y'])
+            zlist.append(star['z'])
+
+    # x, y and z means
+    meanx = np.mean(xlist)
+    meany = np.mean(ylist)
+    meanz = np.mean(zlist)
+    print "mean cluster coordinates for original catalogue:",meanx,meany,meanz
+
+    #INITIAL GUESSES (hardcoded)
+    if fileName == 'Hyades':
+        # Initial Guess for Hyades
+        initial_guess =  [ 46.35158593 , 3.41991717 ,  5.50180935 ,  2.29546342 ,  2.54500714,   3.0  , 4.2 ,  6.0 ,  7.0 ,  7.2,   0.39008765 ,  0.32244944,   0.63084539 , 0.34296494 ,0,0,10,10]
+#    if fileName == 'PleiadesGeneva':
+        # Initial Guess for Pleiades
+#        initial_guess = [  1.25252094e+02 ,  3.35150575e+00,   4.86142031e+00 ,  1.03207132e+01,   1.31350468e+01 , -2.39384409e+00 ,  1.96855446e-01 ,  3.04079043e+00,   4.22140557e+00 ,  5.37379397e+00 ,  1.55576559e+00,   4.49615136e-01,   2.19895913e-01 ,  1.67554634e-01 ,  0,0,0.1,0.1]
+    if fileName == 'PleiadesGeneva':
+        # Initial Guess for Pleiades
+        initial_guess = [  1.25252094e+02 ,  3.35150575e+00,   4.86142031e+00 ,  1.03207132e+01,   1.31350468e+01 , -2.39384409e+00 ,  1.96855446e-01 ,  3.04079043e+00,   4.22140557e+00 ,  5.37379397e+00 ,  1.55576559e+00,   4.49615136e-01,   2.19895913e-01 ,  1.67554634e-01 ,  0,0,0.1,0.1]
+    if fileName== "Alpha_persei_1": 
+        initial_guess =[173.57,	3.42,	5.50,	2.30,	2.55,	-0.50,	2.0,	3,	4.0,	5.5,	0.20,	0.20,	0.20,	0.20,	0.0227700,	-0.0254000,	0.0018400,	0.0015100]
+    if fileName== "Blanco1": 
+        initial_guess = [231.14,	3.42,	5.50,	2.30,	2.55,	1.3,	2.5,	3.8,	4.5,	5.0,	0.20,	0.20,	0.20,	0.20,	0.0185130,	0.0025400,	0.0008200,	0.0008200]
+    if fileName== "Col140_1":
+        initial_guess = [351.30,	3.42,	5.50,	2.30,	2.55,	-2.0, 2.0,2.8,	3.0,3.4,	0.20,	0.20,	0.20,	0.20,	-0.0083700,	0.0048500,	0.0006100,	0.0007300]
+    if fileName== "Coma_berenices_1":
+        initial_guess = [85.19,	3.42,	5.50,	2.30,	2.55,	1.5,	3.5,5.0,6.0,6.5,	0.20,	0.20,	0.20,	0.20,	-0.0120600,	-0.0090800,	0.0013700,	0.0011600,]
+    if fileName== "IC2391":
+        initial_guess = [145.35,	3.42,	5.50,	2.30,	2.55,	0.0,1.8,	3.2,	4.4,	5.0,	0.20,	0.20,	0.20,	0.20,	-0.0241600,	0.0233054,	0.0027600,	0.0027290]
+    if fileName== "IC2602_1":
+        initial_guess = [149.46,	3.42,	5.50,	2.30,	2.55,	0.5,	3.1,	4.9,	5.0,	5.5,	0.20,	0.20,	0.20,	0.20,	-0.0174800,	0.0109300,	0.0022600,	0.0030900]
+    if fileName== "IC4665_1":
+        initial_guess = [358.84,	3.42,	5.50,	2.30,	2.55,	-0.5,	0.5,	1.5,	2.8, 3.9,	0.20,	0.20,	0.20,	0.20,	-0.0007800,	-0.0082200,	0.0003200,	0.0002900]
+    if fileName== "NGC2232":
+        initial_guess=[329.85,	3.42,	5.50,	2.30,	2.55,	-1.0,	1.1,	2.0,	3.5,	4.1,	0.20,	0.20,	0.20, 0.20,	-0.0044600,	-0.0016900,	0.0004400,	0.0005700]
+    if fileName== "NGC2422_1":
+        initial_guess = [456.93,	3.42,	5.50,	2.30,	2.55,	-1.0, 1.5, 3.0, 3.5,	4.0,	0.20,	0.20,	0.20,	0.20,	-0.0066800,	0.0009100,	0.0004700,	0.0003200]
+    if fileName== "NGC2451_1":
+        initial_guess = [184.65,	3.42,	5.50,	2.30,	2.55,	-0.5,	1.5,	3.0,	4.5,	5.5,	0.20,	0.20,	0.20,	0.20,	-0.0213200,	0.0156500,	0.0012100,	0.0011100]
+    if fileName== "NGC2516_1":
+        initial_guess = [355.68,	3.42,	5.50,	2.30,	2.55,	-1.5,	0.00,	2.0,	3.0,	3.2,	0.20,	0.20,	0.20,	0.20,	-0.0038600,	0.0108000,	0.0005500,	0.0004900]
+    if fileName== "NGC2547":
+        initial_guess = [364.66,	3.42,	5.50,	2.30,	2.55,	-0.0,	1.20,	2.0,	2.8,	3.20,	0.20,	0.20,	0.20,	0.20,	-0.0088700,	0.0041500,	0.0003900,	0.0002700]
+    if fileName== "NGC3532_1":
+        initial_guess = [450.06,	3.42,	5.50,	2.30,	2.55,	0.0,	1.10,	1.6,	2.8,	3.3,	0.20,	0.20,	0.20,	0.20,	-0.0103900,	0.0052500,	0.0004100,	0.0003700]
+    if fileName== "NGC6475":
+        initial_guess = [280.22,	3.42,	5.50,	2.30,	2.55,	-1.0,	2.0,	2.5,	3.5,	4.0,	0.20,	0.20,	0.20,	0.20,	0.0030600,	-0.0053500,	0.0005900,	0.0005500]
+    if fileName== "NGC6633_1":
+        initial_guess = [410.52,	3.42,	5.50,	2.30,	2.55,	0.5,	1.0,	2.0,	3.2,	3.5,	0.20,	0.20,	0.20,	0.20,	0.0015100,	-0.0017600,	0.0005130,	0.0002500]
+        #initial_guess = [410.52,	3.42,	5.50,	2.30,	2.55,	0.5,2.5,3.5,	0.20,	0.20,	0.20,	0.20,	0.0015100,	-0.0017600,	0.0005130,	0.0002500]
+    if fileName== "NGC7092_1":
+        initial_guess = [309.32,	3.42,	5.50,	2.30,	2.55,	-0.5,	0.5,	2.0,	2.5,	2.9,	0.20,	0.20,	0.20,	0.20,	-0.0070900,	-0.0199100,	0.0005800,	0.0004800]
+    if fileName== "Praesepe_1":
+        initial_guess = [183.97,	3.42,	5.50,	2.30,	2.55,	0.00,	2.1,	3.8,	4.1,	4.5,	0.20,	0.20,	0.20,	0.20,	-0.0361600,	-0.0130600,	0.0016300,	0.0012700]
+        
+     
+    # Find number of stars in every bin (color bin)
+    w=findW(starlist)
+
+    print "Number of stars in cluster",len(starlist)
+    c={'N':len(starlist),'Q':500,'mlim':20.0,'lmean':calculateMeanL(starlist, len(starlist)),'bmean':calculateMeanB(starlist, len(starlist)),"w":w}
+
+    # Calculates ML and errors
+    results = MLE(starlist, c, initial_guess)
+    calculateErrors(starlist, c, results)
